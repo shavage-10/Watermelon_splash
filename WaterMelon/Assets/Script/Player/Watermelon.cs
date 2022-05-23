3eb2959098b24efa9d30a4a0c19674537abd7517
shavage-10 <99919208+shavage-10@users.noreply.github.com>
2022-05-23 12:50:48 +0200

    game

---
+++ WaterMelon/Assets/Script/Player/Watermelon.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Watermelon : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("hit detected");
+    }
+
+
+
+
+
+
+}
