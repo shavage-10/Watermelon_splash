40e835232b6c41561c37b67574aee7b1b78f9fc1
shavage-10 <99919208+shavage-10@users.noreply.github.com>
2022-05-23 15:44:30 +0200

    Game over part

---
+++ WaterMelon/Assets/Scripts/Events.cs
@@ -0,0 +1,15 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Events : MonoBehaviour
+{
+  public void ReplayGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
