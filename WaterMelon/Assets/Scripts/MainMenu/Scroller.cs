7866ecd59b322951dc0b7d758f6a86c2efa9e0c0
Jeswer <kost@itu.dk>
2022-05-24 21:15:50 +0200

    Update Scroller.cs

--- WaterMelon/Assets/Scripts/MainMenu/Scroller.cs
+++ WaterMelon/Assets/Scripts/MainMenu/Scroller.cs
@@ -9,6 +9,6 @@ public class Scroller : MonoBehaviour
     [SerializeField] private float _x, _y;
     void Update()
     {
-        _img.uvRect = new Rect(_img.uvRect.position + new Vector2(_x, _y * Time.deltaTime, _img.uvRect.size);
+        _img.uvRect = new Rect(_img.uvRect.position + new Vector2(_x, _y) * Time.deltaTime, _img.uvRect.size);
     }
 }
