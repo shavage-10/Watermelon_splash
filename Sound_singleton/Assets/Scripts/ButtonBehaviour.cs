69629a062bd5de608d33b3c81b5c0088ad52669b
shavage-10 <shra@itu.dk>
2022-05-24 12:10:49 +0200

    Singleton

---
+++ Sound_singleton/Assets/Scripts/ButtonBehaviour.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonBehaviour : MonoBehaviour{
+
+    int n;
+
+    public void InstatiateCaller(GameObject AmongUs)
+    {
+        Instantiate(AmongUs);
+    }
+}
+
