a3bf383e75ca16bb30286e4ab71e3b3f32dd8a90
Jeswer <kostecki.jesper@gmail.com>
2022-05-25 15:20:52 +0200

    organization and slider

---
+++ WaterMelon/Assets/Scripts/MainMenu/Settings/VolumeSlider.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeSlider : MonoBehaviour
+{
+    [SerializeField] private Slider _slider;
+    void Start()
+    {
+        _slider.onValueChanged.AddListener(val => AudioManager.Instance.ChangeMasterVolume(val));
+
+    }
+
+    
+    
+}
