c73b2d9b2159aff88efdcdcfc1125c02b3e1ca97
Jeswer <kostecki.jesper@gmail.com>
2022-05-25 20:23:11 +0200

    Scoreboard check + New icons + Settings

--- WaterMelon/Assets/Scripts/MainMenu/Settings/VolumeSlider.cs
+++ WaterMelon/Assets/Scripts/MainMenu/Settings/VolumeSlider.cs
@@ -8,12 +8,8 @@ public class VolumeSlider : MonoBehaviour
     [SerializeField] private Slider _slider;
     void Start()
     {
-        DontDestroyOnLoad(_slider);
         AudioManager.Instance.ChangeMasterVolume(_slider.value);
         _slider.onValueChanged.AddListener(val => AudioManager.Instance.ChangeMasterVolume(val));
 
     }
-
-    
-    
 }
